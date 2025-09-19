4c26892615c62dea1f71b6d1c5d309ccc476a9da
Yuri Edward <yuri6037@outlook.com>
2025-09-19 23:38:22 +0200

    Fixed bug with product bundle identifier

---
+++ BPXEdit/include/BPXEdit/BPXSection.h
@@ -0,0 +1,16 @@
+//
+//  BPXSection.h
+//  BPXEdit
+//
+//  Created by Yuri Edward on 9/19/25.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BPXSection : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ BPXEdit/include/BPXEdit/BPXSection.m
@@ -0,0 +1,12 @@
+//
+//  BPXSection.m
+//  BPXEdit
+//
+//  Created by Yuri Edward on 9/19/25.
+//
+
+#import "BPXSection.h"
+
+@implementation BPXSection
+
+@end
