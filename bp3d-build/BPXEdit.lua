58da24eac8af02b0dbdb1d4a65ddfc5d9b4a410f
Yuri Edward <yuri6037@outlook.com>
2026-02-27 23:53:06 +0100

    Added support for ios simulator target

--- bp3d-build/BPXEdit.lua
+++ bp3d-build/BPXEdit.lua
@@ -15,6 +15,8 @@ local function build1(ctx, args)
     bp3d.util.table.concat(args, { "VALID_ARCHS=arm64", "-scheme", "BPXEdit" })
     if bp3d.util.utf8.contains(ctx.target, "darwin") then
         bp3d.util.table.concat(args, { "-destination", "generic/platform=macOS" })
+    elseif bp3d.util.utf8.contains(ctx.target, "ios-sim") then
+        bp3d.util.table.concat(args, { "-destination", "generic/platform=iOS Simulator" })
     elseif bp3d.util.utf8.contains(ctx.target, "ios") then
         bp3d.util.table.concat(args, { "-destination", "generic/platform=iOS" })
     end
@@ -33,6 +35,8 @@ function BPXEdit:prePackage(ctx, artifacts)
     build1(ctx, { "SYMROOT=" .. bp3d.build.files.toString(path) })
     if bp3d.util.utf8.contains(ctx.target, "darwin") then
         artifacts:add(bp3d.build.Artifact.resource(path:join("Debug/BPXEdit.framework"), "BPXEdit.framework"))
+    elseif bp3d.util.utf8.contains(ctx.target, "ios-sim") then
+        artifacts:add(bp3d.build.Artifact.resource(path:join("Debug-iphonesimulator/BPXEdit.framework"), "BPXEdit.framework"))
     elseif bp3d.util.utf8.contains(ctx.target, "ios") then
         artifacts:add(bp3d.build.Artifact.resource(path:join("Debug-iphoneos/BPXEdit.framework"), "BPXEdit.framework"))
     end
